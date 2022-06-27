79469c42624d6f1589c9738e3e6c0a1ada43a1bb
AsrielPires <bebether@gmail.com>
2022-06-27 01:52:24 +0100

    changes

--- crud.ts
+++ crud.ts
@@ -4,14 +4,15 @@ import { div, g, One, S } from "galho";
 import { RecordStyle } from "galhui/list";
 import { defRenderer, FieldPlatform, list as rawList } from "galhui/list";
 import { input, output } from "galhui/io";
-import Table, { TableColumn, TableOption } from "galhui/table";
-import { byKey, isF, sub, t } from "inutil";
-import { copy, set } from "orray";
+import Table, { Column, Option } from "galhui/table";
+import { byKey, isF, l, sub, t } from "inutil";
+import { copy } from "orray";
 import { card } from "./card";
 import { size } from "./fields";
 import { mdPost, mdPut } from "./form";
 import { all, ctxMenu, tryRemove } from "./tools";
 import { ibutton, $, C } from "galhui";
+import { wait } from "galhui/wait";
 
 interface IEttCrud {
   add?(): any;
@@ -28,8 +29,6 @@ interface IEttCrud {
 
 export interface ilist extends IEttCrud {
   item?: (value: Rec) => any;
-  head?: S<HTMLTableSectionElement>;
-  foot?: S<HTMLTableSectionElement>;
   // options?: ((item: Rec, index: number) => One)[];
 }
 export function list(bond: Bond, i: ilist | ((value: Rec) => any) = {}) {
@@ -39,8 +38,6 @@ export function list(bond: Bond, i: ilist | ((value: Rec) => any) = {}) {
     open: i.open || i.edit || ((value) => mdPut(bond.target, value?.id)),
     remove: (...value) => tryRemove(bond.target, sub(value, "id")),
     menu: () => ctxMenu(bond, { edit: (i as ilist).edit }),
-    head: i.head,
-    foot: i.foot,
     // options: i.options,
     item: i.item || card(bond)
   }, bond.bind());
@@ -49,26 +46,32 @@ export function list(bond: Bond, i: ilist | ((value: Rec) => any) = {}) {
 
 export interface itable extends IEttCrud {
   iform?: bool;
-  options?: TableOption<Dic>[];
+  options?: Option<Dic>[];
   p?: FieldPlatform;
   style?: RecordStyle;
 }
 export function table(bond: Bond, i: itable = {}) {
   let
     ent = bond.target,
-    allColumns = ent.fields.map((f: Field) => {
+    req = [ent.main],
+    allColumns = ent.fields.map((f: Field): Column => {
       let tp = fieldTypes[f.tp];
-      return <TableColumn>{
-        key: f.key,
-        text: f.text,//ett$.w(ent, "f", f.key),
-        align: tp.align,
-        size: size(f),
+      return {
+        opts: f,
         tp: tp.dt,
+        key: f.key,
+        text: f.text,
+        size: size(f),
+        align: tp.align,
         fmt: tp.output?.bind(f),
-        opts: f
       }
     });
-  bond.fields.length || bond.fields.set(sub(fields(ent, (f: Field) => !f.side), "key"));
+  if (i.iform) {
+    req.push(...sub(ent.fields.filter(v => v.req), "key"));
+    for (let field of req)
+      bond.fields.includes(field) || bond.fields.push(field);
+  }
+  l(bond.fields) || bond.fields.set(sub(fields(ent, (f: Field) => !f.side), "key"));
   return new Table<Dic>({
     sort: {
       clear: true,
@@ -77,31 +80,29 @@ export function table(bond: Bond, i: itable = {}) {
     single: i.single,
     p: i.p || defRenderer,
     corner: output(all(bond)),
-    open: i.open || i.edit || ((value) => mdPut(ent, value?.id)),
+    style: i.style || ent.style,
+    options: i.options, resize: true,
+    allColumns, reqColumns: req, key: "id",
+    columns: copy(bond.fields, v => byKey(allColumns, v)),
     remove: (...value) => tryRemove(ent, sub(value, 'id')),
     menu: t(i.menu) && (() => ctxMenu(bond, { edit: i.edit })),
-    options: i.options,
-    resize: true,
-    style: i.style || ent.style,
-    allColumns,
-    key: "id",
-    columns: copy(bond.fields, v => byKey(allColumns, v))
-  }, bond.bind());
+    open: i.open || i.edit || ((value) => mdPut(ent, value?.id)),
+  }, bond.bind(), [fromArray(allColumns, v => [v.key, v.input()])]);
 }
 
-export async function single(key: str) {
-  let ent = await entity(key);
-  return [
-    list(new Bond(ent, ["name", "info"]), {
-      item: v => [
-        g("td", "bd", [v.name, v.info && (`(${v.info})`)]),
-        g("td", 0, ibutton($.i.edit, null, () => mdPut(ent, v.id)).cls(C.extra))
-      ],
-      foot: g("tr", 0, [
-        g("td"),
-        g("td", 0, input("text", "name", "Designação")),
-        g("td", 0, ibutton($.i.plus, null, () => mdPost(ent))),
-      ]),
-    })
-  ];
-}
+// export async function single(key: str) {
+//   let ent = await entity(key);
+//   return [
+//     list(new Bond(ent, ["name", "info"]), {
+//       item: v => [
+//         g("td", "bd", [v.name, v.info && (`(${v.info})`)]),
+//         g("td", 0, ibutton($.i.edit, null, () => mdPut(ent, v.id)).cls(C.extra))
+//       ],
+//       foot: g("tr", 0, [
+//         g("td"),
+//         g("td", 0, input("text", "name", "Designação")),
+//         g("td", 0, ibutton($.i.plus, null, () => mdPost(ent))),
+//       ]),
+//     })
+//   ];
+// }
